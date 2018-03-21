d47d98eae841a1a694f45df0d66a3a52b4fdac77
Andrew <andbostick@gmail.com>
2018-03-20 17:47:15 -0700

    Validation finished

--- app.js
+++ app.js
@@ -18,6 +18,19 @@ UI.prototype.addBookToList = function(book) {
 
 }
 
+UI.prototype.showAlert =function(message, className){
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+
+    setTimeout(function() {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
 UI.prototype.clearFields = function() {
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
@@ -34,9 +47,14 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     const book = new Book(title, author, isbn);
     //Instantiate UI
     const ui = new UI();
+    if(title === '' || author === '' || isbn === ''){
+        ui.showAlert('Please fill in all fields', 'error')
+    } else {
+        ui.addBookToList(book);
+        ui.showAlert('Book Added!', 'success');
+        ui.clearFields();
+    }
     
-    ui.addBookToList(book);
-    ui.clearFields();
 
 
     e.preventDefault();
